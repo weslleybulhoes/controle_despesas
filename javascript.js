1f40a97ee7a4dd19e9daa61b9bccd4cb07607a12
weslley bulhoes <weslley.bulhoes@gmail.com>
2020-10-04 03:11:05 -0300

    primeiros passos

---
+++ javascript.js
@@ -0,0 +1,29 @@
+
+class Despesa {
+    constructor(ano, mes, dia, tipo, descricao, valor) {
+        this.ano = ano
+        this.mes = mes
+        this.dia = dia
+        this.tipo = tipo
+        this.descricao = descricao
+        this.valor = valor
+    }
+}
+
+function cadastrandoUsuario () {
+    let ano = document.getElementById("ano").value
+    let mes = document.getElementById("mes").value
+    let dia = document.getElementById("dia").value
+    let tipo = document.getElementById("tipo").value
+    let descricao = document.getElementById("descricao").value
+    let valor = document.getElementById("valor").value
+
+    let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+    console.log(despesa)
+
+}
+
+
+
+
+
